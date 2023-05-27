76dc6550351e1197ed528b4e6c11b734a4bcf2d7
Arukuen <adsarmiento3@up.edu.ph>
2023-05-27 21:36:41 +0800

    fix: simulate pzem measurements

--- IoMeter.ino
+++ IoMeter.ino
@@ -9,6 +9,10 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // Iniatialize PZEM to pins 16 and 17
 PZEM004Tv30 pzem(Serial2, 16, 17);
 
+//Simlating power and energy
+double power = 0;
+double energy = 0;
+
 void setup() {
     // Establish serial communication with baud rate 115200
     Serial.begin(115200);
@@ -28,12 +32,16 @@ void loop() {
     // Get all the measurements
     float voltage = pzem.voltage();
     float current = pzem.current();
-    float power = pzem.power();
-    float energy = pzem.energy();
+    // float power = pzem.power();
+    // float energy = pzem.energy();
     float frequency = pzem.frequency();
     float pf = pzem.pf();
 
+    // Simulating the power and energy
+    power = 60;
+    energy += ((power / (3600/2)) / 1000);
     float cost = energy * 10;
+
 
     // Check if the measurements are valid
     if(isnan(voltage)) {
@@ -49,27 +57,28 @@ void loop() {
     } else if (isnan(pf)) {
         Serial.println("Error reading power factor");
     } else {
-        // Print the measurements to the Serial monitor
-        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
-        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
-        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
-        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
-        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
-        Serial.print("PF: ");           Serial.println(pf);
-        Serial.println();
-
-        // Display Power and Energy
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("P: ");
-        lcd.print(power);
-        lcd.print(" E: ");
-        lcd.print(energy, 3);
-        lcd.print("kWh");
-        // Display the cost
-        lcd.setCursor(0, 1);
-        lcd.print("Cost: Php");
-        lcd.print(cost);
-        delay(2000);
+        // Put here the printing and display
     }
+
+    // Print the measurements to the Serial monitor
+    Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+    Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+    Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+    Serial.print("Energy: ");       Serial.print(energy,5);     Serial.println("kWh");
+    Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+    Serial.print("PF: ");           Serial.println(pf);
+    Serial.println();
+
+    // Display Power and Energy
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("P:");
+    lcd.print(power, 0);
+    lcd.print("  E:");
+    lcd.print(energy, 5);
+    // Display the cost
+    lcd.setCursor(0, 1);
+    lcd.print("Cost: Php");
+    lcd.print(cost);
+    delay(2000);
 }
